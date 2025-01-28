cbaac5f87e21574d592482f5ca05bd77ef368d9e
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2025-01-28 18:43:26 +0100

    create project structure

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,13 @@
+import Contact from "../Contact/Contact";
+
+const ContactList = () => {
+  return (
+    <div>
+      <Contact />
+      <Contact />
+      <Contact />
+    </div>
+  );
+};
+
+export default ContactList;
